649897870de323710eed54cb00ffcba6d9aa43d0
codingpratham <coding.job17@gmail.com>
2026-06-06 23:44:28 +0530

    testing done tiome to integrate socket.io

--- backend/src/routes/index.r.ts
+++ backend/src/routes/index.r.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import authRouter from './auth.r.js';
+import userRouter from './user.r.js';
 
 const router = express.Router();
 
 router.use('/auth',authRouter);
+router.use('/user',userRouter);
 
 export default router;
