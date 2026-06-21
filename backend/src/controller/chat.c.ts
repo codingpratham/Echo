3944663736c0eb38d1d435f4fbe2511fa74946ca
codingpratham <coding.job17@gmail.com>
2026-06-21 23:41:12 +0530

    .

--- backend/src/controller/chat.c.ts
+++ backend/src/controller/chat.c.ts
@@ -2,6 +2,9 @@ import socket from 'socket.io';
 
 export const chatController = (io: socket.Server) => {
     io.on('connection', (socket) => {
-        console.log('a user connected');  
+        console.log('a user connected');
+        socket.rooms.add('chatroom');
+          
     });
+
 }
